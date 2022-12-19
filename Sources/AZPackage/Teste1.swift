e86c930499a14cac0cbf46ebf97a6ecec45ba67d
Pedro Azevedo <>
2022-12-19 16:35:36 -0300

    testing files

---
+++ Sources/AZPackage/Teste1.swift
@@ -0,0 +1,8 @@
+//
+//  File 2.swift
+//  
+//
+//  Created by Pedro Azevedo on 19/12/22.
+//
+
+import Foundation
---
+++ Sources/AZPackage/Teste2.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Pedro Azevedo on 19/12/22.
+//
+
+import Foundation
